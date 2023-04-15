716d2fb79811f52c0c64cc2d6533cdba0b02dee5
Sean Moen <spaulm5@gmail.com>
2023-04-15 16:24:24 -0500

    Made GoogleMapsAPI call a function

--- googlemaps.py
+++ googlemaps.py
@@ -3,38 +3,33 @@ import json
 import fileinput
 import pandas
 
+
 for line in fileinput.input(files = "config.env"):
     key = line
 
-#This wil get the gas stations in x meters around the longitude and latitude
-type = "gas%20station"
-latitude = "37.95098545221008"
-longitude = "-91.77840922372175"
-#url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "%2C" + longitude + "&radius=" + str(radius) + "%40" + latitude + "%2C" + longitude + "&fields=formatted_address%2Cname%2Copening_hours%2Cgeometry&key=" + key
-url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "%2C" + longitude + "&type=" + type + "&keyword=" + type + "&rankby=distance" + "&key=" + key
+def get_gas_stations():
+    #This wil get the gas stations around the longitude and latitude
+    type = "gas%20station" #%20 is escape for space
+    latitude = "37.95098545221008"
+    longitude = "-91.77840922372175"
 
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "%2C" + longitude + "&type=" + type + "&keyword=" + type + "&rankby=distance" + "&key=" + key
+    payload={}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response_string = json.loads(response.text)
 
-payload={}
-headers = {}
+    gas_stations = []
+    for gas_station in response_string['results']:
+        gas_stations.append(gas_station['name'])
+    return gas_stations
 
-response = requests.request("GET", url, headers=headers, data=payload)
+print(get_gas_stations())
 
-#print(response.text)
-
-
-
-response_string = json.loads(response.text)
-
-#print(response_string)
-
-gas_stations = []
-for gas_station in response_string['results']:
-    gas_stations.append(gas_station['name'])
-
-print(gas_stations)
 #Output- pandas data frame
 #column 1 - name
 #column 2 - distance
 #list for Carter
 #dictionary with appropriate data
+
 
