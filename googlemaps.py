8aea643639e03275d2533d70e46c05d473081bad
Sean Moen <spaulm5@gmail.com>
2023-04-15 18:28:26 -0500

    Organized code and now checks if gas station is open

--- googlemaps.py
+++ googlemaps.py
@@ -1,35 +1,31 @@
 import requests
 import json
 import fileinput
-import pandas
 
-
+#Read in API key from config.env file
 for line in fileinput.input(files = "config.env"):
     key = line
 
-def get_gas_stations():
-    #This wil get the gas stations around the longitude and latitude
+#Returns a list of nearby gas stations given latitude and longitude as string
+#The closest gas station is listed first
+#Default location is the Gale Bullman building
+def get_gas_stations(latitude = "37.95098545221008", longitude = "-91.7784092232175"):
     type = "gas%20station" #%20 is escape for space
-    latitude = "37.95098545221008"
-    longitude = "-91.77840922372175"
 
+    #GoogleMaps API works by making an HTTPS request with input parameters in the url link
     url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "%2C" + longitude + "&type=" + type + "&keyword=" + type + "&rankby=distance" + "&key=" + key
     payload={}
     headers = {}
     response = requests.request("GET", url, headers=headers, data=payload)
+    #Convert JSON to a string
     response_string = json.loads(response.text)
 
     gas_stations = []
     for gas_station in response_string['results']:
-        gas_stations.append(gas_station['name'])
+        #Check if GoogleMapsAPI reports the gas station as open or not
+        try:
+            if gas_station["opening_hours"]["open_now"]:
+                gas_stations.append(gas_station['name'])
+        except KeyError: #GoogleMapsAPI does not report if this gas station is open, assume open
+            gas_stations.append(gas_station['name'])
     return gas_stations
-
-print(get_gas_stations())
-
-#Output- pandas data frame
-#column 1 - name
-#column 2 - distance
-#list for Carter
-#dictionary with appropriate data
-
-
